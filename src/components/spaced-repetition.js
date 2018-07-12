a09a03e2629f403f90187db2242099e750068750
patersog <gianluca.paterson@gmail.com>
2018-07-12 12:39:16 -0400

    Updated spaced repetition component

--- src/components/spaced-repetition.js
+++ src/components/spaced-repetition.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export const SpacedRepetition = (props) => {
-	return (
-		<div>Here is the prompt: {props.prompt}</div>
-	);
-};
+export class SpacedRepetition extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.propmt = props.prompt;
+
+		this.state = {
+			answer:''
+		};
+	}
+	render() {
+		return (
+			<div>Here is the prompt: {prompt}</div>
+		);
+	}
+}
 
 export default SpacedRepetition;
