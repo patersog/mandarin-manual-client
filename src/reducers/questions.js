431d919f370e2f28623787c5e21227b55c0d20bc
patersog <gianluca.paterson@gmail.com>
2018-07-12 17:40:12 -0400

    Update action/reducer for questions

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,25 +1,27 @@
 import {
 	FETCH_QUESTION_SUCCESS,
-	FETCH_QUESTION_ERROR
+	FETCH_QUESTION_ERROR,
 } from '../actions/action-types';
 
 const initialState = {
 	prompt: '',
 	correct: false,
 	loading: false,
-	error: null
+	error: null,
+
 };
 
 export default function reducer(state = initialState, action) {
 	if (action.type === FETCH_QUESTION_SUCCESS) {
-		console.log(action);
 		return Object.assign({}, state, {
 			prompt: action.prompt,
+			loading: action.loading,
 			error: null
 		});
 	} else if (action.type === FETCH_QUESTION_ERROR) {
 		return Object.assign({}, state, {
-			error: action.error
+			error: action.error,
+			loading: action.loading
 		});
 	}
 	return state;
